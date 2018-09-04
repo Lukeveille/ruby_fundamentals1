d819e9c5b4cb3ec536fd30eba16d5f145dbc27a1
Lukeveille <luke@lukeleveille.com>
2018-09-04 15:57:20 -0400

    Completed exercise 2

--- exercise2.rb
+++ exercise2.rb
@@ -1,4 +1,13 @@
 print "How much was your bill? "
 bill = gets.chomp.to_f
 
-puts "You should be tipping $#{(bill * 0.2).round(3)} on a $#{bill} bill."
+puts "You should be tipping $#{(bill * 0.2).round(2)} on $" + bill.to_s
+
+n1 = 
+n2 = 
+
+puts "the result of 45628 multiplied by 7839 is " + (45628 * 7839).to_s
+
+puts "(10 < 20 && 30 < 20) || !(10 == 11) is true, and I can prove it below"
+
+puts (10 < 20 && 30 < 20) || !(10 == 11)
