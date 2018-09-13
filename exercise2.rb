7bb1dadfc2cbc3897a5e61bea120759c66301fbe
Lukeveille <luke@lukeleveille.com>
2018-09-13 10:07:42 -0400

    Slight refactor as per rubocop

--- exercise2.rb
+++ exercise2.rb
@@ -3,11 +3,10 @@ bill = gets.chomp.to_f
 
 puts "You should be tipping $#{(bill * 0.2).round(2)} on $" + bill.to_s
 
-n1 = 
-n2 = 
-
 puts "the result of 45628 multiplied by 7839 is " + (45628 * 7839).to_s
 
 puts "(10 < 20 && 30 < 20) || !(10 == 11) is true, and I can prove it below"
 
-puts (10 < 20 && 30 < 20) || !(10 == 11)
+exp = (10 < 20 && 30 < 20) || !(10 == 11)
+
+puts exp
